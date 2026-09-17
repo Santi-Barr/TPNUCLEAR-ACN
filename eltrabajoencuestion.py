55c4942f31f085755e4ebe701bf0d3fe146adc15
julydev144hz <ippolito.julianet36@gmail.com>
2026-09-17 13:46:17 -0300

    santi terminalo chabon

--- eltrabajoencuestion.py
+++ eltrabajoencuestion.py
@@ -11,7 +11,21 @@ np.random.uniform()
 
 avion = [[0, 0, 0, 0, 0] for _ in range(25)]
 
-asientos = set(range(25*4))
+asientos = set()
+asientosVentanas = set()
+
+stevens = [[]*8]
+
+
+for i in range(25*4):
+    if(i%4 == 3 or i%4 == 0):
+        asientosVentanas.add(i)
+        if(i%4 == 3):
+
+            stevens[0].append(i)
+        else:
+            stevens[0]
+    asientos.add(i)
 fila_actual = 24
 libres = [-2,-1,1,2]
 
@@ -25,6 +39,34 @@ def entraUnPajeroRandom():
     asientos.remove(nuevoPasajero)
     tieneCarryOn = bool(np.random.binomial(n=1, p=PROBABILIDAD_CARRYON, size=1)[0])
 
+    return {"posActual" : (0, 0), "dest" : (int(fila), int(columna)), "carryon" : tieneCarryOn, "esperar" : 0, "bajando" : 0, "sentando" : 0}
+
+def entraUnPajeroWILMA():
+    nuevoPasajero = -1
+    if len(asientosVentanas) > 0:
+        nuevoPasajero = np.random.choice(list(asientosVentanas))
+        asientosVentanas.remove(nuevoPasajero)
+    else:
+        nuevoPasajero = np.random.choice(list(asientos))
+    fila = nuevoPasajero//4
+    columna = nuevoPasajero%4
+    if(columna <=1): columna-=2
+    else: columna-=1
+    asientos.remove(nuevoPasajero)
+    tieneCarryOn = bool(np.random.binomial(n=1, p=PROBABILIDAD_CARRYON, size=1)[0])
+
+    return {"posActual" : (0, 0), "dest" : (int(fila), int(columna)), "carryon" : tieneCarryOn, "esperar" : 0, "bajando" : 0, "sentando" : 0}
+
+def entraUnPajeroB2FUltimate():
+    ultimos = list(asientos)[-20::]    
+    nuevoPasajero = np.random.choice(ultimos)
+    fila = nuevoPasajero//4
+    columna = nuevoPasajero%4
+    if(columna <=1): columna-=2
+    else: columna-=1
+    asientos.remove(nuevoPasajero)
+    tieneCarryOn = bool(np.random.binomial(n=1, p=PROBABILIDAD_CARRYON, size=1)[0])
+    
     return {"posActual" : (0, 0), "dest" : (int(fila), int(columna)), "carryon" : tieneCarryOn, "esperar" : 0, "bajando" : 0, "sentando" : 0}
 
 def entraUnPajeroB2F():
@@ -45,6 +87,10 @@ def entraUnPajeroB2F():
     
     return {"posActual" : (0, 0), "dest" : (int(fila_actual), int(columna)), "carryon" : tieneCarryOn, "esperar" : 0, "bajando" : 0, "sentando" : 0}
 
+def entraEstiven():
+
+    pass
+
 
 pajeros = []
 
@@ -57,7 +103,7 @@ while(len(asientos) > 0 or (t>0 and len(pajeros) > 0)):
     #Esto depende de la politca (random)
 
     if(avion[0][2] == 0 and len(asientos) > 0 and len(pajeros) < 4):
-        pajeros.append(entraUnPajeroB2F())
+        pajeros.append(entraUnPajeroB2FUltimate())
         
     # print(pajeros)
     for pajero in pajeros:
